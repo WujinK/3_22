3c8949e320474c1d834abb002b192a87b4c67f01
WujinK <kimwj0728@naver.com>
2022-04-06 22:13:51 +0900

    4_6

--- 4_6.py
+++ 4_6.py
@@ -1,1 +1,8 @@
-print ("hello")
+# find_element_by_id => 아이디 속성 사용하여 접근
+# find_element_by_name => name 속성
+# find_element_by_xpath => xpath 속성
+# find_element_by_link_text => 앵커태그(a태그)에 사용되는 텍스트로 접근
+# find_element_by_partial_link_text => 앵커태그(a태그)에 사용되는 일부 텍스트로 접근
+# find_element_by_tag_name => 태그를 사용해서 접근
+# find_element_by_class_name => 클래스 이용해서 접근
+# find_element_by_css_selector => css 선택자 사용해서 접근
