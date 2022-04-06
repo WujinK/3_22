6240fc0b1236e476128f8a153b20ca9deedf8837
WujinK <kimwj0728@naver.com>
2022-04-06 23:00:57 +0900

    4_6

--- 4_6.py
+++ 4_6.py
@@ -5,4 +5,10 @@
 # find_element_by_partial_link_text => 앵커태그(a태그)에 사용되는 일부 텍스트로 접근
 # find_element_by_tag_name => 태그를 사용해서 접근
 # find_element_by_class_name => 클래스 이용해서 접근
-# find_element_by_css_selector => css 선택자 사용해서 접근
+ 
+from selenium import webdriver
+chrome_path = r"C:\\py_wj\\chromedriver_win32"
+url = 'https://www.naver.com/'
+browser = webdriver.Chrome(chrome_path)
+browser.get(url)
+browser.find_element_by_xpath('//*[@id="account"]/a').click()
